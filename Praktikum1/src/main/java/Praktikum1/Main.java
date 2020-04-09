70cb3d86ad6279b51fa9ba5096bc60ad95e63759
Gian Gifarly <62729477+giangifarly@users.noreply.github.com>
2020-04-09 17:13:44 +0700

    Add files via upload

---
+++ Praktikum1/src/main/java/Praktikum1/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Praktikum1;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Adhitya Jhonlin S
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame futama = new JFrame();
+        futama.setSize(400,300);
+        futama.setTitle("Program GUI Pertamaku");
+        futama.setVisible(true);
+        
+    }
+    
+}
